e9cbcb4f520e312377b3d0fa1af654392f076b86
Valentine Silvansky <v.silvansky@gmail.com>
2016-07-21 10:56:10 +0300

    Initial version

--- KeyboardShowHideTest/ViewController.m
+++ KeyboardShowHideTest/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+
 @end
 
 @implementation ViewController
@@ -17,11 +22,62 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
+}
+
+- (IBAction)cancelClicked:(id)sender {
+	[self.textField resignFirstResponder];
+}
+
+#pragma mark - Keyboard Notifications
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+	CGFloat keyboardHeight = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+	self.bottomConstraint.constant = keyboardHeight;
+	self.heightConstraint.constant = 44;
+
+	[self.view setNeedsUpdateConstraints];
+
+	NSTimeInterval duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue] * 0.9f;
+
+	NSInteger opts = [notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+	[UIView animateWithDuration:duration delay:0.f options:opts << 16 animations:^{
+		[self.view layoutIfNeeded];
+	} completion:NULL];
+
+}
+
+- (void)keyboardDidShow:(NSNotification *)notification
+{
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+	self.bottomConstraint.constant = 44;
+	self.heightConstraint.constant = 60;
+
+	[self.view setNeedsUpdateConstraints];
+
+	NSTimeInterval duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue] * 0.9f;
+
+	NSInteger opts = [notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+	[UIView animateWithDuration:duration delay:0.f options:opts << 16 animations:^{
+		[self.view layoutIfNeeded];
+	} completion:NULL];
+
+}
+
+- (void)keyboardDidHide:(NSNotification *)notification
+{
 }
 
 @end
